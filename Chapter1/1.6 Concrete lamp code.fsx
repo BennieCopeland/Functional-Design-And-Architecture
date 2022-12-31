e73188861484077c435625762b4a67fa308cdd1e
Bennie Copeland <bennie.r.copeland@gmail.com>
2022-12-31 19:05:25 +0900

    Add listing 1.6

---
+++ Chapter1/1.6 Concrete lamp code.fsx
@@ -0,0 +1,24 @@
+﻿type ILampSwitcher =
+    abstract member Switch: bool -> unit
+
+///////////////////////
+// Begin Listing 1.6 //
+///////////////////////
+type DaylightLamp (n: string, v: int, onOff: bool) =
+    let mutable isOn = onOff
+    let mutable value = v
+    let name = n
+    
+    interface ILampSwitcher with
+        member this.Switch onOff =
+            isOn <- onOff
+
+type TableLamp (n: string, onOff: bool) =
+    let mutable isOn = onOff
+    let name = n
+    
+    interface ILampSwitcher with
+        member this.Switch onOff =
+            isOn <- onOff
+            // Debug: will remove it later!
+            failwith "Switched"
