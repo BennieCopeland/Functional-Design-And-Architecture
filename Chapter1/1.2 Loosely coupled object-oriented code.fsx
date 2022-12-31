47ec85df35ede59f86ba60a899391e7e2ac35f3a
Bennie Copeland <bennie.r.copeland@gmail.com>
2022-12-31 13:51:48 +0900

    Add listing 1.2

---
+++ Chapter1/1.2 Loosely coupled object-oriented code.fsx
@@ -0,0 +1,16 @@
+﻿// Listing 1.2
+type ISensor =
+    abstract member GetData : unit -> float
+    abstract member GetName : unit -> string
+    abstract member GetDataType : unit -> string
+
+type IConnection =
+    abstract member Send : (string * string * float) -> unit
+
+[<Sealed>]
+type Observer(sensor: ISensor, connection: IConnection) =
+    member this.ReadAndSendData () =
+        let data = sensor.GetData()
+        let sensorName = sensor.GetName()
+        let dataType = sensor.GetDataType()
+        connection.Send(sensorName, dataType, data)
