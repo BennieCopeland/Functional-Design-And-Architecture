8c17a783c193e513fd0591d2edc01c69190b5453
Bennie Copeland <bennie.r.copeland@gmail.com>
2022-12-31 19:12:22 +0900

    Add listing 2.1

---
+++ Chapter 02/2.1 Domain-specific language for a smart house.fsx
@@ -0,0 +1,26 @@
+﻿///////////////////////
+// Begin Listing 2.1 //
+///////////////////////
+type ThermometerName = string
+type BarometerName = string
+
+type Method =
+    | ReadThermometer of ThermometerName
+    | ReadBarometer of BarometerName
+    | ReportTemperature
+    | ReportAtmospherePressure
+    | ClearData
+
+type Language = Method list
+
+let script =
+    [
+        ReadThermometer "Garage"
+        ReadThermometer "Near the road"
+        ReadThermometer "House"
+        ReadBarometer "Garage"
+        ReadBarometer "House"
+        ReportTemperature
+        ReportAtmospherePressure
+        ClearData
+    ]
