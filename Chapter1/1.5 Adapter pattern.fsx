1222818ef4c1476de4695e1d6ba378c052a1b5cb
Bennie Copeland <bennie.r.copeland@gmail.com>
2022-12-31 18:47:16 +0900

    Add listing 1.5

---
+++ Chapter1/1.5 Adapter pattern.fsx
@@ -0,0 +1,46 @@
+﻿module Native =
+    module Core =
+        module HighAccuracyThermometer =
+            type Temperature =
+                private
+                | Kelvin of float
+                | Celsius of float
+            
+            let (|Kelvin|Celsius|) (temperature: Temperature) =
+                match temperature with
+                | Kelvin v -> Kelvin v
+                | Celsius v -> Celsius v
+            
+            let getData () =
+                Kelvin 0.0
+        
+        module Utils =
+            let toCelsius (data) : float =
+                match data with
+                | HighAccuracyThermometer.Kelvin v -> 273.15 - v
+                | HighAccuracyThermometer.Celsius v -> v
+
+type ISensor =
+    abstract member GetData : unit -> float
+    abstract member GetName : unit -> string
+    abstract member GetDataType : unit -> string
+
+///////////////////////
+// Begin Listing 1.5 //
+///////////////////////
+[<Sealed>]
+type HighAccuracyThermometer =
+    member this.Name() = "HAT-53-2"
+    member this.GetKelvin() =
+        Native.Core.HighAccuracyThermometer.getData()
+
+[<Sealed>]
+type HAThermometerAdapter (thermometer: HighAccuracyThermometer) =
+    let t = thermometer
+    
+    interface ISensor with
+        member this.GetData () =
+            let data = t.GetKelvin()
+            Native.Core.Utils.toCelsius data
+        member this.GetName () = t.Name()
+        member this.GetDataType () = "temperature"
