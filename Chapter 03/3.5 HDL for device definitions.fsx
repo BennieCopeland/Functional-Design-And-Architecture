310e9f4aafcdede07923179daa0fdd35b1d26abc
Bennie Copeland <bennie.r.copeland@gmail.com>
2023-01-03 21:59:45 +0900

    Add listing 3.5

---
+++ Chapter 03/3.5 HDL for device definitions.fsx
@@ -0,0 +1,34 @@
+﻿///////////////////////
+// Begin Listing 3.5 //
+///////////////////////
+module Andromeda =
+    module Hardware =
+        module Common =
+            // Every physical instance of a component has its own GUID
+            type PhysicalGuid = string
+
+            // Components are grouped by name,
+            // for example, temperature sensors AAA-T-25
+            type ComponentName = string
+
+            type Parameter      = Temperature | Pressure
+            type ComponentClass = Sensors | Controllers
+            type Vendor         = string
+
+            type ComponentPassport = {
+                ComponentClass : ComponentClass
+                ComponentName : ComponentName
+                ComponentGuid : PhysicalGuid
+                ComponentVendor : Vendor
+            }
+
+    module Hdl =
+        open Hardware.Common
+    
+        type ComponentIndex = string
+        
+        type ComponentDef =
+            | Sensor of (ComponentPassport * ComponentIndex * Parameter)
+            | Controller of (ComponentPassport * ComponentIndex)
+        
+        type Hdl = ComponentDef list
